8f6e22d57a0383baaa1c7125a7bb59ab4dd4fd67
Westindiess <gregoryalexiss@gmail.com>
2022-01-14 13:13:47 +0100

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="text-center bg-blue-600 text-white p-5 lg:p-16">
+    <div className="text-center text-white bg-blue-600  p-5 lg:p-16">
       <h1 className="text-5xl lg:text-8xl">Google Font™</h1>
       <p className="text-lg pt-3 lg:text-2xl">
         Le plus récentes, les plus trendy et les plus populaires polices Google.
